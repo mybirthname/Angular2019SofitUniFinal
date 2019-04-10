31fa178cfb6b8f46e0053e772d080bfa9709f6eb
Martin Stanchev <martin.stanchev87@gmail.com>
2019-04-11 01:50:46 +0300

    Working version without ngrx toaster

--- src/app/core/app-http-error.interceptor.ts
+++ src/app/core/app-http-error.interceptor.ts
@@ -1,12 +1,12 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {tap, catchError} from 'rxjs/operators';
-import {ToastrService} from '../../../node_modules/toastr'
+
 
 export class AppHttpErrorInterceptor implements HttpInterceptor
 {
 
-    constructor(private toaster: ToastrService){
+    constructor(){
 
     }
 
@@ -15,7 +15,6 @@ export class AppHttpErrorInterceptor implements HttpInterceptor
         return next.handle(req.clone()).pipe(tap((success)=>{
           }),
           catchError((err)=>{
-              this.toaster.error(err.error.message, 'Error');
               throw err;
             }));
     }    
